86b9e6e14f3249f77da40d3afc21495edbed8883
connorwake <connor.wake@gmail.com>
2015-07-17 11:45:24 -0700

    Basic neural net functioning
    Fixed basic bugs and basic tests run

--- neural_net.py
+++ neural_net.py
@@ -1,48 +1,123 @@
 # A class to represent a neural net for the NEAT algorithm
 
 
-class NEATComponent:
-	genetic_marker_control = 0
+
 
 class NeuralNet:
 
 	def __init__(self):
-		self.nodes = []
+		self.hiddenNodes = []
+		self.sensorNodes = []
+		self.outputNodes = []
 	 	self.connections = []
 
 
-	class Node(NEATComponent):
-		def __init__(self, weight, category):
-			self.weight = weight
-			self.category = category
-			self.genetic_marker = self.genetic_marker_control
-			self.genetic_marker_control += 1
+	def createHiddenNode(self, threshold):
+		self.hiddenNodes.append(Node(threshold))
 
-		def setWeight(self, weight):
-			self.weight = weight
+	def createOutputNode(self, threshold):
+		self.outputNodes.append(outputNode(threshold))
 
-		def getWeight(Self):
-			return self.weight
-
-	class Connection(NEATComponent):
-		def  __init__(self, weight, start, end, enabled):
-			self.weight = weight
-			self.start = start
-			self.end = end
-			self.enabled = enabled
-			self.genetic_marker = self.genetic_marker_control
-			self.genetic_marker_control += 1
-
-		def setWeight(self, weight):
-			self.weight = weight
-
-		 def getWeight(self):
-		 	return self.weight
-
-	def createNode(self, weight, category):
-		self.nodes.append(self.Node(weight, category))
+	def createSensorNode(self, threshold):
+		self.sensorNodes.append(Node(threshold))
 
 	def createConnection(self, weight, start, end, enabled):
-		self.connections.append(self.Connection(weight, start, end, enabled))
+		self.connections.append(Connection(weight, start, end, enabled))
 
-	
+	def process(self, sensor_readings):
+		for i in range(0,len(self.sensorNodes)):
+			self.sensorNodes[i].send(sensor_readings[i])
+		return [node.out for node in self.outputNodes]
+
+
+
+class NEATComponent:
+	genetic_marker_control = 0
+
+
+class Node(NEATComponent):
+	def __init__(self, threshold):
+		self.threshold = threshold
+		self.genetic_marker = self.genetic_marker_control
+		self.genetic_marker_control += 1
+		self.input = []
+		self.output = []
+
+		self.mailbox = []
+
+		self.ready = False
+
+	def setThreshold(self, threshold):
+		self.threshold = threshold
+
+	def getThreshold(self):
+		return self.threshold
+
+	def addInput(self, x):
+		self.input.append(x)
+
+	def addOutput(self, x):
+		self.output.append(x)
+
+	def send(self, package):
+		for connection in self.output:
+			connection.send(package)
+
+	def receive(self, package):
+		self.mailbox.append(package)
+		if len(self.mailbox) == len(self.input):
+			tot = 0
+			for mail in self.mailbox:
+				tot += mail
+			self.mailbox = []
+			if tot >= self.getThreshold():
+				self.send(1)
+			else:
+				self.send(0)
+
+
+
+
+class hiddenNode(Node):
+	def send(self, package):
+		for connection in self.output:
+			connection.send(package)
+
+class outputNode(Node):
+	def __init__(self, threshold):
+		Node.__init__(self,threshold)
+		self.ready = False
+		self.out = None
+
+	def send(self, package):
+		self.ready = True
+		self.out = package
+
+
+
+
+
+
+class Connection(NEATComponent):
+	def  __init__(self, weight, start, end, enabled):
+		self.weight = weight
+		self.start = start
+		self.end = end
+		self.enabled = enabled
+		self.genetic_marker = self.genetic_marker_control
+		self.genetic_marker_control += 1
+
+		end.addInput(self)
+		start.addOutput(self)
+
+	def setWeight(self, weight):
+		self.weight = weight
+
+	def getWeight(self):
+		return self.weight
+
+	def send(self, package):
+		if self.enabled:
+			self.end.receive(package*self.getWeight())
+		else:
+			self.end.receive(0)
---
+++ neural_net_tests.py
@@ -0,0 +1,18 @@
+import neural_net, unittest
+
+
+
+test_net = neural_net.NeuralNet()
+
+for i in range(0,5):
+	test_net.createSensorNode(0)
+	test_net.createOutputNode(2)
+	test_net.createConnection(1,test_net.sensorNodes[i], test_net.outputNodes[i], True)
+
+print test_net.process([1,1,5,1,1])
+
+test_net.createHiddenNode(0)
+test_net.createConnection(1,test_net.sensorNodes[0],test_net.hiddenNodes[0], True)
+test_net.createConnection(1,test_net.hiddenNodes[0],test_net.outputNodes[0], True)
+
+print test_net.process([1,1,1,1,1])
